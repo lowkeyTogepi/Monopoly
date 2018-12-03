7adaca4b7372a21d01746388f60928739f6b16b1
lilhappyburrito <45431125+lilhappyburrito@users.noreply.github.com>
2018-12-03 11:13:34 -0700

    Add files via upload

---
+++ Monopoly.java
@@ -0,0 +1,11 @@
+package edu.neumont.csc110;
+
+import java.io.IOException;
+
+public class Monopoly {
+
+	public static void main(String[] args) throws IOException, NoSuchFieldException {
+		Game game = new Game();
+		game.printStartMenu();
+	}
+}
