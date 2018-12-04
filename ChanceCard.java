6fe4005f5ba31c9f2346de7dede659dc37d90f94
lilhappyburrito <45431125+lilhappyburrito@users.noreply.github.com>
2018-12-04 08:17:21 -0700

    Add files via upload

---
+++ ChanceCard.java
@@ -0,0 +1,5 @@
+package edu.neumont.csc110;
+
+public class ChanceCard {
+	String[] chanceCards = new String[16];
+}
---
+++ CommunityChestCard.java
@@ -0,0 +1,5 @@
+package edu.neumont.csc110;
+
+public class CommunityChestCard {
+	String[] communityChestCard = new String[17];
+}
